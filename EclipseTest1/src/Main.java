b3c1ab6f59a780ffe7a1707f2e44511c9a9ab1df
KT <kamil.tajak@gmail.com>
2023-09-06 15:18:55 +0200

    Added class Figura.java

--- EclipseTest1/src/Main.java
+++ EclipseTest1/src/Main.java
@@ -4,10 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Hello");
 		
-		for(int i = 0; i < 10; i++) System.out.println("Hello");
-		for(int i = 0; i < 10; i++) System.out.println("Hello");
 	}
 
 }
