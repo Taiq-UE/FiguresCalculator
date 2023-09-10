729aac83795a39627fd8901dacaeea3562ba2de8
KT <kamil.tajak@gmail.com>
2023-09-10 16:11:12 +0200

    Figura: Kwadrat, Prostokat, Kolo

--- EclipseTest1/src/Figura.java
+++ EclipseTest1/src/Figura.java
@@ -1,6 +1,108 @@
-
 public class Figura 
 {
+	protected double dA;
+	
+	Figura(double a)
+	{
+		dA = a;
+	}
 	
 	
+	double Pole(){
+		return 0;
+	}
+	double Obwod(){
+		return 0;
+	}
+	public String toString()
+	{
+		return "Pole: " + Pole() + "\nObwod: " + Obwod();
+	}
+}
+
+class Kwadrat extends Figura
+{
+	
+	Kwadrat(double dA)
+	{
+		super(dA);
+	}
+	
+	double Pole() 
+	{
+		return dA * dA;
+	}
+	double Obwod()
+	{
+		return (4*dA);
+	}
+	double Przekatna()
+	{
+		return (dA*Math.sqrt(2));
+	}
+	
+	public String toString()
+	{
+		return super.toString() +
+				"\nPrzekatna: " + Przekatna();
+	}
+ }
+
+class Prostokat extends Kwadrat
+{
+	double dB;
+	
+	Prostokat(double dA, double b)
+	{
+		super(dA);
+		dB = b;
+	}
+	
+	double Pole()
+	{
+		return dA * dB;
+	}
+	double Obwod()
+	{
+		return (2*dA) + (2*dB);
+	}
+	double Przekatna()
+	{
+		double a2, b2, wynik;
+		a2 = dA*dA;
+		b2 = dB*dB;
+		wynik = a2 + b2;
+		wynik = Math.sqrt(wynik);
+	
+		return wynik;
+	}
+	
+}
+
+
+class Kolo extends Figura
+{
+	Kolo(double dA)
+	{
+		super(dA);
+	}
+	
+	double Pole()
+	{
+		return Math.PI*dA*dA;
+	}
+	double Obwod()
+	{
+		return 2*Math.PI*dA;
+	}
+	double Przekatna()
+	{
+		return 2*dA;
+	}
+	
+	public String toString()
+	{
+		return super.toString() +
+				"\nPrzekatna: " + Przekatna();
+	}
 }
--- EclipseTest1/src/Main.java
+++ EclipseTest1/src/Main.java
@@ -1,10 +1,18 @@
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("Hello");
+	public static void main(String[] args) 
+	{
+		Prostokat p1 = new Prostokat(2,4);
+		Kwadrat k1 = new Kwadrat(2);
+		Kolo ko1 = new Kolo(3);
+		//System.out.println(p1.Pole());
+		System.out.println(p1.toString()+"\n");
+		System.out.println(k1.toString()+"\n");
+		System.out.println(ko1.toString());
+		
+		
 	}
 
 }
